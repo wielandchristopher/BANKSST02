11ae0f74fecbfe2e092504d7e1120c4962cbaf51
Kaschi92 <stephanie.kaschnitz@gmx.at>
2016-11-02 09:56:22 +0100

    Fehlerbehebung doppeltes Attribut

--- Eigene_Bank_DLL_Assembly/Eigene_Bank_DLL_Assembly/BankManagement.cs
+++ Eigene_Bank_DLL_Assembly/Eigene_Bank_DLL_Assembly/BankManagement.cs
@@ -19,7 +19,7 @@ namespace Eigene_Bank_DLL_Assembly
         [DllImport(path, CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr readUser(int id);
 
-        [DllImport(path, EntryPoint = "searchUser", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(path, CallingConvention = CallingConvention.Cdecl)]
         public static extern int searchUser(String vorname, String nachname, String geb);
 
         [DllImport(path, CallingConvention = CallingConvention.Cdecl)]
